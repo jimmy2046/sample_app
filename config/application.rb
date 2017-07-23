e41ae284a4f177684509a6a0d7d5b82a2614f2bd
jimmy2046 <jimmychong_st@yahoo.com>
2017-07-22 18:03:21 -0700

    Add user following

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,4 @@
 require_relative 'boot'
-
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
@@ -11,5 +10,6 @@ module SampleApp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+                  
   end
 end
